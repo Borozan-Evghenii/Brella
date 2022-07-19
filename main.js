78a8e97c15de0be2e944200ed4f5f8129c1949ac
Korben-Dallas <borozan.eugen@gmail.com>
2022-07-19 10:30:14 +0300

    Thirt commit
    *ADD Tabs

---
+++ main.js
@@ -0,0 +1,9 @@
+// Tabs for services section
+const servicesButtons = document.querySelectorAll(".services-list__item");
+const servicesContent = document.querySelectorAll(
+  ".services-description__content"
+);
+
+servicesButtons.addEventListener("click", (element) => {
+  console.log(element);
+});
